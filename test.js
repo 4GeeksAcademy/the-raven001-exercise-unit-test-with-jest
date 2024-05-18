85bdbb1236ef31066435f1ba90c871be353f2864
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-05-18 05:55:16 +0000

    Final commit for tests

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 test("One euro should be 1.07 dollars", function() {
 
-    const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
+    const { fromEuroToDollar } = require('./app.js');
+
+     expect(fromEuroToDollar(1)).toBe(1.07);
+
+});
 
 
-    const dollars = fromEuroToDollar(3.5);
+test("One dollar should be 145.26 Yens", function(){
+    const {fromDollarToYen} = require("./app.js");
+
+    expect(fromDollarToYen(1)).toBe(146.26168224299064)
+});
 
 
-    const expected = 3.5 * 1.07; 
-    
+test("One Yen should be 0.005 Pounds", function(){
+    const {fromYenToPound} = require("./app.js");
 
-     expect(fromEuroToDollar(3.5)).toBe(3.745);
+    expect(fromYenToPound(1)).toBe(0.005495207667731629)
+});
 
-     expect(fromDollarToYen(3.5)).toBe(511.91588785046724)
-
-     expect(fromYenToPound(250)).toBe(1.3738019169329074)
-})
